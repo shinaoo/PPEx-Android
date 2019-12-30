2b35b5b285a82ac7dec0f78d2c129e0bfab4d757
125859320@qq.com <125859320@qq.com>
2019-12-30 11:29:34 +0800

    ThrougTypeMsgHandler修改适应Rudp2"

--- app/src/main/java/ppex/client/handlers/ThroughTypeMsgHandler.java
+++ app/src/main/java/ppex/client/handlers/ThroughTypeMsgHandler.java
@@ -18,6 +18,7 @@ import ppex.client.rudp.ClientOutput;
 import ppex.proto.entity.Connection;
 import ppex.proto.entity.through.Connect;
 import ppex.proto.entity.through.RecvInfo;
+import ppex.proto.msg.Message;
 import ppex.proto.msg.type.ThroughTypeMsg;
 import ppex.proto.msg.type.TypeMessage;
 import ppex.proto.msg.type.TypeMessageHandler;
@@ -90,13 +91,16 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
                 rudpPack = addrManager.get(connections.get(0).getAddress());
                 if (rudpPack == null) {
                     IOutput output = new ClientOutput(channel, connections.get(0));
-                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+//                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+                    rudpPack = RudpPack.newInstance(output,Client.getInstance().getExecutor(),Client.getInstance().getResponseListener(),addrManager);
                     Client.getInstance().getAddrManager().New(connections.get(0).getAddress(), rudpPack);
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
-                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
+//                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
                 } else {
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
                 }
 
                 Log.e("MyTag", "------------->hole_punch SND connect ping normal");
@@ -104,7 +108,7 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
                 ttmsg.setContent(JSON.toJSONString(connect));
 
                 rudpPack = addrManager.get(Client.getInstance().getAddrServer1());
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
             } else if (connect.getType() == Connect.TYPE.RETURN_HOLE_PUNCH.ordinal()) {
                 //开始给B 发ping消息
                 connect.setType(Connect.TYPE.CONNECT_PING_NORMAL.ordinal());
@@ -112,13 +116,16 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
                 rudpPack = addrManager.get(connections.get(1).getAddress());
                 if (rudpPack == null) {
                     IOutput output = new ClientOutput(channel, connections.get(1));
-                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+//                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+                    rudpPack = RudpPack.newInstance(output,Client.getInstance().getExecutor(),Client.getInstance().getResponseListener(),addrManager);
                     addrManager.New(connections.get(1).getAddress(), rudpPack);
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
-                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
+//                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
+//                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
                 } else {
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
                 }
                 Log.e("MyTag", "------------->return_hole_punch SND connect ping normal");
             } else if (connect.getType() == Connect.TYPE.REVERSE.ordinal()) {
@@ -127,20 +134,24 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
                 rudpPack = addrManager.get(connections.get(0).getAddress());
                 if (rudpPack == null) {
                     IOutput output = new ClientOutput(channel, connections.get(0));
-                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+//                    rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+                    rudpPack = RudpPack.newInstance(output,Client.getInstance().getExecutor(),Client.getInstance().getResponseListener(),addrManager);
                     addrManager.New(connections.get(0).getAddress(), rudpPack);
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
-                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
+//                    RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
+//                    Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
                 } else {
-                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                    rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                    rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
                 }
                 Log.e("MyTag", "------------->reverse SND connect ping reverse");
             } else if (connect.getType() == Connect.TYPE.FORWARD.ordinal()) {
                 connect.setType(Connect.TYPE.RETURN_FORWARD.ordinal());
                 ttmsg.setContent(JSON.toJSONString(connect));
                 rudpPack = addrManager.get(Client.getInstance().getAddrServer1());
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
                 //forward的接收方已经表示连接了
 //                EventBus.getDefault().post(new BusEvent(BusEvent.Type.THROUGN_RCV_CONNECT_PONG.getValue()));
                 Log.e("MyTag", "------------->forward");
@@ -148,7 +159,8 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
                 connect.setType(Connect.TYPE.CONNECTED.ordinal());
                 ttmsg.setContent(JSON.toJSONString(connect));
                 rudpPack = addrManager.get(Client.getInstance().getAddrServer1());
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
 
                 //这个是判断forward类型是否已经连接成功
                 Client.getInstance().setConnTarget(connections.get(1));
@@ -172,14 +184,16 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
             rudpPack = addrManager.get(connections.get(0).getAddress());
             if (rudpPack == null) {
                 IOutput output = new ClientOutput(channel, connections.get(0));
-                rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+//                rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+                rudpPack = RudpPack.newInstance(output,Client.getInstance().getExecutor(),Client.getInstance().getResponseListener(),addrManager);
                 addrManager.New(connections.get(0).getAddress(), rudpPack);
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-                RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
-                Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
+//                RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
+//                Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
             } else {
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
             }
             Log.e("MyTag", "----------->rcv connectping normal return connect pong");
             //todo 之前有一个ConnectedMap保存已经连接上的连接.现在去掉了,之后再考虑如何
@@ -197,13 +211,16 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
             rudpPack = addrManager.get(connections.get(1).getAddress());
             if (rudpPack == null) {
                 IOutput output = new ClientOutput(channel, connections.get(1));
-                rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+//                rudpPack = new RudpPack(output, Client.getInstance().getExecutor(), Client.getInstance().getResponseListener());
+                rudpPack = RudpPack.newInstance(output,Client.getInstance().getExecutor(),Client.getInstance().getResponseListener(),addrManager);
                 addrManager.New(connections.get(1).getAddress(), rudpPack);
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
-                RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
-                Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
+//                RudpScheduleTask rudpScheduleTask = new RudpScheduleTask(Client.getInstance().getExecutor(), rudpPack, addrManager);
+//                Client.getInstance().getExecutor().executeTimerTask(rudpScheduleTask, rudpPack.getInterval());
             } else {
-                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//                rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+                rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
             }
             Log.e("MyTag", "----------->rcv connectping reverse return connect pong");
         } else if (connect.getType() == Connect.TYPE.CONNECT_PONG.ordinal()) {
@@ -222,7 +239,8 @@ public class ThroughTypeMsgHandler implements TypeMessageHandler {
             ttmsg.setContent(JSON.toJSONString(connect));
 
             rudpPack = addrManager.get(Client.getInstance().getAddrServer1());
-            rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+//            rudpPack.write(MessageUtil.throughmsg2Msg(ttmsg));
+            rudpPack.send2(MessageUtil.throughmsg2Msg(ttmsg));
         } else {
             throw new Exception("Client handle unknown connect operate:" + connect.toString());
         }
